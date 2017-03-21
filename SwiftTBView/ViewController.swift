199241b2c7fb8b5c6e6d216abfa51d7e32ac8aa1
hkshen <308421189@qq.com>
2017-03-21 15:19:18 +0800

    Initial Commit

---
+++ SwiftTBView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTBView
+//
+//  Created by kun shen on 2017/3/21.
+//  Copyright © 2017年 kun shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
